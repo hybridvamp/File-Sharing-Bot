ab694923f99daa7aa9d0aa98921b7ca7f186313b
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-12-18 16:27:39 +0400

    s

--- plugins/useless.py
+++ plugins/useless.py
@@ -4,6 +4,7 @@ from pyrogram import filters
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT
 from datetime import datetime
 from helper_func import get_readable_time
+from database.database import full_userbase
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
