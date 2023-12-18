d912d1f4a0c982e94f385fe2b0000f8bb60ae73a
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2023-12-18 16:30:26 +0400

    a

--- plugins/useless.py
+++ plugins/useless.py
@@ -14,24 +14,23 @@ async def stats(bot: Bot, message: Message):
 
 @Bot.on_message(filters.command("list") & filters.user(ADMINS))
 async def get_users_ids(client, message: Message):
-    reply_message = await message.reply_text("`Checking users count...`")
+    reply_message = await message.reply_text("Checking users count...")
     user_ids = await full_userbase()    
-    users_file = "\n".join(user_ids)
     
-    await reply_message.edit_text(f"`{len(user_ids)} users found on db`")
-    await reply_message.edit_text(f"`{len(user_ids)} users found on db` \n**Creating userlist file...**")
+    await reply_message.edit_text(f"{len(user_ids)} users found on db")
+    await reply_message.edit_text(f"{len(user_ids)} users found on db \n**Creating userlist file...**")
     
-    file_message = await message.reply_text("`Creating userlist file...`")
+    file_message = await message.reply_text("Creating userlist file...")
     with open("user_ids.txt", "w") as file:
         for i, user_id in enumerate(user_ids):
             file.write(user_id + "\n")
             if (i + 1) % 1000 == 0:
-                await file_message.edit_text(f"`Creating userlist file... {i+1}/{len(user_ids)}`")
+                await file_message.edit_text(f"Creating userlist file... {i+1}/{len(user_ids)}")
     
-    await file_message.edit_text("`Userlist file created.`")
-    
+    await file_message.edit_text("Userlist file created.")
     await client.send_document(chat_id=message.chat.id, document="user_ids.txt", file_name="user_ids.txt")
-    
+    await file_message.delete()
+    await reply_message.delete()
     os.remove("user_ids.txt")
 
 @Bot.on_message(filters.private & filters.incoming)
