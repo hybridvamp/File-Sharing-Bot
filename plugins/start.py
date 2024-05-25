f3a45147c34b7d0ea7cc42e0494ab5839b8a0b7b
HYBRID <48980248+hybridvamp@users.noreply.github.com>
2024-05-25 18:36:49 +0530

    forward option

--- plugins/start.py
+++ plugins/start.py
@@ -181,48 +181,93 @@ async def get_users(client: Bot, message: Message):
 @Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
 async def send_text(client: Bot, message: Message):
     if message.reply_to_message:
-        query = await full_userbase()
-        broadcast_msg = message.reply_to_message
-        total = 0
-        successful = 0
-        blocked = 0
-        deleted = 0
-        unsuccessful = 0
-        
-        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
-        for chat_id in query:
-            try:
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except FloodWait as e:
-                await asyncio.sleep(e.x)
-                await broadcast_msg.copy(chat_id)
-                successful += 1
-            except UserIsBlocked:
-                await del_user(chat_id)
-                blocked += 1
-            except InputUserDeactivated:
-                await del_user(chat_id)
-                deleted += 1
-            except:
-                unsuccessful += 1
-                pass
-            total += 1
-        
-        status = f"""<b><u>Broadcast Completed</u>
+        args = message.text.split()
+        if len(args) == 1:
+            query = await full_userbase()
+            broadcast_msg = message.reply_to_message
+            total = 0
+            successful = 0
+            blocked = 0
+            deleted = 0
+            unsuccessful = 0
+            
+            pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+            for chat_id in query:
+                try:
+                    await broadcast_msg.copy(chat_id)
+                    successful += 1
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    await broadcast_msg.copy(chat_id)
+                    successful += 1
+                except UserIsBlocked:
+                    await del_user(chat_id)
+                    blocked += 1
+                except InputUserDeactivated:
+                    await del_user(chat_id)
+                    deleted += 1
+                except:
+                    unsuccessful += 1
+                    pass
+                total += 1
+            
+            status = f"""<b><u>Broadcast Completed</u>
 
-Total Users: <code>{total}</code>
-Successful: <code>{successful}</code>
-Blocked Users: <code>{blocked}</code>
-Deleted Accounts: <code>{deleted}</code>
-Unsuccessful: <code>{unsuccessful}</code></b>"""
-        
-        return await pls_wait.edit(status)
+    Total Users: <code>{total}</code>
+    Successful: <code>{successful}</code>
+    Blocked Users: <code>{blocked}</code>
+    Deleted Accounts: <code>{deleted}</code>
+    Unsuccessful: <code>{unsuccessful}</code></b>"""
+            
+            return await pls_wait.edit(status)
+        elif len(args) == 2:
+            fwd = message.command[1]
+            if not fwd == "-f":
+                msg = await message.reply(REPLY_ERROR)
+                await asyncio.sleep(8)
+                await msg.delete()
+            query = await full_userbase()
+            broadcast_msg = message.reply_to_message
+            total = 0
+            successful = 0
+            blocked = 0
+            deleted = 0
+            unsuccessful = 0
+            
+            pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+            for chat_id in query:
+                try:
+                    await broadcast_msg.forward(chat_id)
+                    successful += 1
+                except FloodWait as e:
+                    await asyncio.sleep(e.x)
+                    await broadcast_msg.forward(chat_id)
+                    successful += 1
+                except UserIsBlocked:
+                    await del_user(chat_id)
+                    blocked += 1
+                except InputUserDeactivated:
+                    await del_user(chat_id)
+                    deleted += 1
+                except:
+                    unsuccessful += 1
+                    pass
+                total += 1
+            
+            status = f"""<b><u>Broadcast Completed</u>
 
+    Total Users: <code>{total}</code>
+    Successful: <code>{successful}</code>
+    Blocked Users: <code>{blocked}</code>
+    Deleted Accounts: <code>{deleted}</code>
+    Unsuccessful: <code>{unsuccessful}</code></b>"""
+            
+            return await pls_wait.edit(status)
     else:
         msg = await message.reply(REPLY_ERROR)
         await asyncio.sleep(8)
         await msg.delete()
+
 
 @Bot.on_message(filters.private & filters.command('send') & filters.user(ADMINS))
 async def send_message_to_chat(client: Bot, message: Message):
