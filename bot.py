9cdecf5028f056556172aaf8490e322da7a881c4
hybridvamp <zainulhibath24@gmail.com>
2024-08-04 11:43:06 +0530

    as

--- bot.py
+++ bot.py
@@ -103,11 +103,58 @@ class Nbot(Client):
             bot_token=FILE_BOT_TOKEN
         )
         self.LOGGER = LOGGER(__name__)
+
+    async def panic(self): # for use in plugins
+        self.LOGGER.info("\nBot Stopped. Join https://t.me/hybrid_chat for support")
+        sys.exit()
+    
+    async def parse_invite_link(self, channel):
+        try:
+            link = (await self.get_chat(channel)).invite_link
+            if not link:
+                await self.export_chat_invite_link(channel)
+                link = (await self.get_chat(channel)).invite_link
+            self.invitelink = link
+        except Exception as a:
+            self.LOGGER.warning(a)
+            self.LOGGER.warning(f"Bot can't export invite link from one of the channels! [ID: {channel}]")
+            self.LOGGER.warning("Please Double check the FORCE_SUB_CHANNELS values and make sure Bot is admin in channel with 'invite users via link' permission.")
+            self.panic()
+
     async def start(self):
         await super().start()
         file_bot_me = await self.get_me()
         temp.FILE_UN = file_bot_me.username
         self.LOGGER.info(f"@{temp.FILE_UN} Bot Running..!\n\nCreated by (c) Hybrid")
+    
+        self.start_timestamp = datetime.now()
+
+        self.force_sub = {
+            "active": bool(FORCE_SUB_CHANNELS)
+        }
+
+        if self.force_sub["active"]:
+            self.force_sub["ids"] = [FORCE_SUB_CHANNELS]
+            # self.force_sub["links"] = [self.parse_invite_link(FORCE_SUB_CHANNELS)] 
+ 
+        try:
+            db_channel = await self.get_chat(CHANNEL_ID)
+            self.db_channel = db_channel
+            test = await self.send_message(chat_id = db_channel.id, text = "Test Message")
+            await test.delete()
+        except Exception as e:
+            self.LOGGER.warning(e)
+            self.LOGGER.warning(f"Make Sure bot is Admin in DB Channel, and Double check the CHANNEL_ID Value, Current Value {CHANNEL_ID}")
+            self.panic()
+
+        self.set_parse_mode(ParseMode.HTML)
+        self.username = file_bot_me.username
+        self.LOGGER.info(f"@{self.username} Bot Running..!")
+        temp.FILE_UN = FILE_BOT_UN
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER.info("Bot stopped.")
 
 async def main():
     bot = Bot()
